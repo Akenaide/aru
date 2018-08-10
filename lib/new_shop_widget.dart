0399c783d036b87776e40ac7dd0f0abb8da5a935
Akenaide <kevinms1@gmail.com>
2018-08-10 10:53:41 +0200

    #2 missing file

---
+++ lib/new_shop_widget.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+import 'package:aru/card.dart';
+
+class NewShopWidget extends StatefulWidget {
+  NewShopWidget(this.shop, this._delete);
+  final Shop shop;
+  final Function(String) _delete;
+
+  @override
+  _NewShop createState() => new _NewShop();
+}
+
+class _NewShop extends State<NewShopWidget> {
+  TextEditingController _nameCtrl = new TextEditingController();
+  TextEditingController _priceCtrl = new TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _nameCtrl.text = this.widget.shop.name;
+    _priceCtrl.text = this.widget.shop.price.toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Card(
+      margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+      elevation: 10.0,
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Row(children: <Widget>[
+
+          const Text(
+            "Shop name",
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          new IconButton(
+            icon: const Icon(Icons.delete),
+            tooltip: 'Delete shop',
+            onPressed: () => this.widget._delete(this.widget.shop.name),
+          )
+          ],),
+          new TextField(
+            controller: _nameCtrl,
+            decoration: new InputDecoration(
+              hintText: "Shop",
+              icon: const Icon(Icons.business),
+            ),
+            maxLength: 10,
+            onChanged: (_val) => setState(() {
+                  this.widget.shop.name = _val;
+                }),
+          ),
+          const Text(
+            "Price",
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          new TextField(
+            controller: _priceCtrl,
+            decoration: new InputDecoration(
+              hintText: "Price",
+              icon: const Icon(Icons.attach_money),
+            ),
+            keyboardType: TextInputType.number,
+            onChanged: (_val) => setState(() {
+                  this.widget.shop.price = int.parse(_val);
+                }),
+          ),
+        ],
+      ),
+    );
+  }
+}
