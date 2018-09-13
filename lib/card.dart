22c776f68820f4a89c755c050da82014e3bca5ec
Akenaide <kevinms1@gmail.com>
2018-09-13 17:59:41 +0200

    #5 Handle image

--- lib/card.dart
+++ lib/card.dart
@@ -1,18 +1,20 @@
 import "dart:convert";
 
 class ShopCard {
-  ShopCard.full(this.cardId, this.stores, this.bought);
+  ShopCard.full(this.cardId, this.stores, this.bought, this.imageurl);
 
   ShopCard.empty(String init) {
     cardId = init;
     bought = false;
     stores = new Map();
+    imageurl = "";
   }
 
   ShopCard.fromStringc(data) {
     var _json = json.decode(data);
     cardId = _json["cardId"];
     bought = _json["bought"];
+    imageurl = _json["imageurl"];
     stores = new Map();
     _json["stores"].forEach((String key, value) {
       stores.putIfAbsent(key, () => value);
@@ -23,6 +25,7 @@ class ShopCard {
     this.cardId = card.id;
     this.bought = false;
     this.stores = {"yyt": card.price};
+    this.imageurl = card.imageUrl;
   }
 
   Map<String, dynamic> toJson() {
@@ -30,6 +33,7 @@ class ShopCard {
       "cardId": this.cardId,
       "bought": this.bought,
       "stores": this.stores,
+      "imageurl": this.imageurl,
     };
   }
 
@@ -58,6 +62,7 @@ class ShopCard {
   String cardId;
   Map<String, int> stores;
   bool bought;
+  String imageurl;
 }
 
 class Shop {
@@ -76,7 +81,7 @@ class CardWS {
   int amount;
   bool selected = false;
   int price;
-  // String imageUrl;
+  String imageUrl;
 
   static List<CardWS> listFromString(String elements) {
     List<CardWS> result = [];
@@ -94,5 +99,6 @@ class CardWS {
     this.id = jCard["ID"];
     this.amount = jCard["Amount"];
     this.price = jCard["Price"];
+    this.imageUrl = jCard["URL"];
   }
 }
