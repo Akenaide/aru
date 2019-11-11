bae2a11c72a860e139040f463b88eef7e8cd0d6e
Akenaide <kevinms1@gmail.com>
2019-11-11 05:17:23 +0100

    Change version and clean

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,10 +1,6 @@
 import 'dart:async';
 import 'package:intl/intl.dart';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-Firestore fsi = Firestore.instance;
-
 final intlNumber = new NumberFormat.decimalPattern("ja");
 final StreamController<String> quantityStream =
     new StreamController.broadcast();
